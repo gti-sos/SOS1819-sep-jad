f5071ee442b00b5f8c5816e92c440eb82f8ed281
Juan Manuel Arteaga <arteagajuanma@gmail.com>
2019-06-05 22:30:36 +0000

    frontend antes de tests

--- public/province-employments-frontend/v1/edit-employments-ctrl.js
+++ public/province-employments-frontend/v1/edit-employments-ctrl.js
@@ -2,9 +2,44 @@
 
 angular
     .module("EmploymentsApp")
-    .controller("EditEmploymentsCtrl",["$scope","$http", function ($scope,$http){
-        console.log("Edit controller initialized");
-        var API = "/api/v1/province-employments";
-        
+    .controller("EditEmploymentsCtrl", ["$scope","$http","$routeParams","$location",
+        function($scope,$http,$routeParams,$location) {
+            console.log("Edit Controller initialized");
+            var API = "/api/v1/province-employments";
+            var province = $routeParams.province;
+            var year = $routeParams.year;
+            var update = true;
+            console.log("Requesting province employment <"+API+"/"+province+"/"+year+">...");
 
-}]);
+            $http.get(API+"/"+province+"/"+year).then(function(response) {
+                console.log("Data Received: "+ JSON.stringify(response.data, null, 2));
+
+                $scope.provinceEmployment = response.data;
+
+            });
+
+            $scope.upupdateProvinceEmployment = function(province,year) {
+                console.log("Updating province employment: "+ JSON.stringify($scope.provinceEmployment));
+
+                Object.keys($scope.provinceEmployment).forEach(d => {
+                    if ($scope.provinceEmployment[d] == "") {
+                        $scope.status = "Error: Faltan parámetros al dato"
+                        update = false;
+                    }
+                })
+                
+                if (update) {
+                    $http
+                        .put(API+"/"+province+"/"+year, $scope.provinceEmployment)
+                        .then(function(response) {
+                                $scope.status = response.status;
+                            },
+                            function(error) {
+                                $scope.status = error.status;
+                            });
+                    $location.path("/ui/v1/province-employments");
+                }
+                update = true;
+            };
+        }
+    ]);
--- public/province-employments-frontend/v1/list-employments-ctrl.js
+++ public/province-employments-frontend/v1/list-employments-ctrl.js
@@ -2,9 +2,126 @@
 
 angular
     .module("EmploymentsApp")
-    .controller("ListEmploymentsCtrl",["$scope","$http", function ($scope,$http){
+    .controller("ListEmploymentsCtrl",["$scope","$http", function($scope,$http) {
         console.log("List controller initialized");
         var API = "/api/v1/province-employments";
+        var limit = 10;
+        var offset = 0;
         
+        refresh();
+
+        function refresh() {
+            console.log("Requesting employments to <"+API+">...");
+            $http.get(API+"?limit="+limit+"&offset="+offset).then(function(response) {
+                console.log("Data received:" + JSON.stringify(response.data, null, 2));
+                
+                $scope.provinceEmployments = response.data;
+            });
+        }
+
+
+        $scope.getNext = function() {
+            $http.get(API+"?limit="+limit+"&offset="+offset).then(function(response) {
+            if ((response.data).length == 10) {
+                offset = offset + 10;
+            }
+            
+            $http.get(API+"?limit="+limit+"&offset="+offset).then(function(response) {  // por que lo repite??
+                $scope.provinceEmployments = response.data;
+            });
+            }, function Error(response) {
+                $scope.provinceEmployments = [];
+            });
+        };
+
+
+        $scope.getBack = function() {
+            if (offset >= 10) {
+                offset = offset - 10;
+            }
+            
+            $http.get(API+"?limit="+limit+"&offset="+offset).then(function(response) {
+                $scope.provinceEmployments = response.data;
+                }, function Error(response) {
+                    $scope.provinceEmployments = [];
+                });
+        };
+   
         
+        $scope.loadProvinceEmployments = function() {
+            $http.get(API+"/loadInitialData").then(function(response) {
+                $scope.status = "BD vacia. Datos iniciales cargados con exito";
+                refresh();
+                }, function(error) {
+                 $scope.status = "BD con datos. No realizada carga inicial de datos";
+                    refresh();
+                });
+        };
+
+
+        $scope.addProvinceEmployment = function() {
+            var newProvinceEmployment = $scope.newProvinceEmployment;
+            console.log("Adding a new provinceEmployment: "+JSON.stringify(newProvinceEmployment));
+            $http
+                .post(API,newProvinceEmployment)
+                .then(function(response) {
+                    console.log("POST response: "+response.status+" "+response.data);
+                    $scope.status = "El recurso "+newProvinceEmployment.province+"-"+newProvinceEmployment.year+" se ha añadido con exito";
+                    refresh();
+                }, function(error) {
+                    if (error.status == 409) {
+                        $scope.status = "Ya existe el recurso "+newProvinceEmployment.province+"-"+newProvinceEmployment.year;
+                        refresh();
+                    } else {
+                        $scope.status = "Alguno de los campos no ha sido rellenado correctamente";
+                        refresh();
+                    }
+                });
+        };
+
+
+        $scope.deleteProvinceEmployment = function(province, year) {
+            console.log("Delete employment with province: "+province+" and year: "+year);
+            $http
+                .delete(API+"/"+province+"/"+year)
+                .then(function(response) {
+                    console.log("DELETE response: "+response.status+" "+response.data);
+                    $scope.status = "El recurso "+province+"-"+year+" se ha borrado con exito";
+                    $scope.getBack();
+                }, function(error) {
+                        $scope.status = "El recurso "+province+"-"+year+" no existe";
+                        refresh();
+                    });
+        };
+
+        $scope.deleteProvinceEmployments = function() {
+            $http
+                .delete(API)
+                .then(function(response) {
+                    $scope.status = "Los recursos se han borrado correctamente";
+                    refresh();
+                });
+        };
+
+
+        $scope.searchByProvince = function() {
+            $http.get(API+"/"+$scope.searchProvince).then(function(response) {
+                console.log("SEARCH response: " + response.status + " " + response.data);
+                
+                $scope.provinceEmployments = response.data;
+                }, function(error) {
+                    $scope.status = "No existe el recurso " + $scope.searchProvince;
+                });
+        };
+
+        $scope.searchByYear = function() {
+            $http.get(API+"?from="+$scope.fromYear+"&to="+$scope.toYear).then(function(response) {
+                console.log("SEARCH response: " + response.status + " " + response.data)
+                
+                $scope.provinceEmployments = response.data;
+            });
+        };
+
+        $scope.limit = limit;
+        $scope.offset = offset;
     }]);
